3e305089d30e4799ae86aea36eab244d034b4022
jonathanvra <jonathan.vra@gmail.com>
2024-08-01 22:09:55 -0500

    Feat: Update layout.js to include DataProvider for context
    The layout.js file was updated to include the DataProvider component of the DataContext.js file. This allows child components to access the data context and use the provided data globally.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,23 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import FloatingMenu from "./components/FloatingMenu";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import FloatingMenu from './components/FloatingMenu';
+import { DataProvider } from './context/dataContext';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "TestTurinng - Jonathan Vera",
-  description: "Prueba técnica para Turinng elaborada por Jonathan Vera",
+  title: 'TestTurinng - Jonathan Vera',
+  description: 'Prueba técnica para Turinng elaborada por Jonathan Vera',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        {children}
-        <FloatingMenu />
+        <DataProvider>
+          {children}
+          <FloatingMenu />
+        </DataProvider>
       </body>
     </html>
   );
