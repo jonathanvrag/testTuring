7650e6ff0b1642ee423dc3a48c7e00699a8e46cb
jonathanvra <jonathan.vra@gmail.com>
2024-08-02 20:00:11 -0500

    Feat: Improve the ModalContent component to represent data in a more organized way
    The ModalContent component was updated to include features that allow for better understanding of the data.

--- src/app/components/ModalContent.jsx
+++ src/app/components/ModalContent.jsx
@@ -5,9 +5,24 @@ function fetchEndpointData(url, setData) {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      setData(prevData => ({ ...prevData, [url]: data.name }));
+      setData(prevData => ({ ...prevData, [url]: data.name || data.title }));
     })
     .catch(error => console.error('Error fetching data:', error));
+}
+
+function formatDate(dateString) {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function renderJSON(data, fetchedData) {
@@ -15,30 +30,86 @@ function renderJSON(data, fetchedData) {
     const entries = Object.entries(data);
     return (
       <ul style={{ listStyleType: 'none', paddingLeft: '20px' }}>
-        {entries.map(([key, value]) => (
-          <li key={key} style={{ marginBottom: '5px' }}>
-            <strong style={{ color: '#333', fontWeight: 'bold' }}>{key}:</strong> {typeof value === 'object' ? renderJSON(value, fetchedData) : (fetchedData[value] || value.toString())}
-          </li>
-        ))}
+        {entries.map(([key, value]) => {
+          const isNumericKey = !isNaN(key);
+          const displayKey = isNumericKey
+            ? '-'
+            : key
+                .replace(/_/g, ' ')
+                .split(' ')
+                .map(capitalizeFirstLetter)
+                .join(' ');
+
+          const displayValue =
+            key === 'created' || key === 'edited'
+              ? formatDate(value)
+              : fetchedData[value] || value.toString();
+
+          return (
+            <li key={key} style={{ marginBottom: '5px' }}>
+              <strong style={{ color: '#333', fontWeight: 'bold' }}>
+                {displayKey}
+                {isNumericKey ? '' : ':'}
+              </strong>{' '}
+              {typeof value === 'object'
+                ? renderJSON(value, fetchedData)
+                : displayValue}
+            </li>
+          );
+        })}
       </ul>
     );
   }
   return <span>{data.toString()}</span>;
 }
 
-export default function ModalContent({ index, title, name, data, handleClose }) {
+export default function ModalContent({
+  index,
+  title,
+  name,
+  data,
+  handleClose,
+}) {
   const [fetchedData, setFetchedData] = useState({});
+  console.log('data: ', data);
 
   useEffect(() => {
     const urls = Object.entries(data)
-      .filter(([key, value]) => key !== 'url' && typeof value === 'string' && value.startsWith('http'))
+      .filter(
+        ([key, value]) =>
+          key !== 'url' && typeof value === 'string' && value.startsWith('http')
+      )
       .map(([key, value]) => value);
+
+    if (Array.isArray(data.films)) {
+      urls.push(...data.films);
+    }
+    if (Array.isArray(data.characters)) {
+      urls.push(...data.characters);
+    }
+    if (Array.isArray(data.planets)) {
+      urls.push(...data.planets);
+    }
+    if (Array.isArray(data.species)) {
+      urls.push(...data.species);
+    }
+    if (Array.isArray(data.starships)) {
+      urls.push(...data.starships);
+    }
+    if (Array.isArray(data.vehicles)) {
+      urls.push(...data.vehicles);
+    }
+    if (Array.isArray(data.pilots)) {
+      urls.push(...data.pilots);
+    }
+
     urls.forEach(url => fetchEndpointData(url, setFetchedData));
   }, [data]);
 
   return (
     <Box
       sx={{
+        height: '80vh',
         position: 'absolute',
         top: '50%',
         left: '50%',
@@ -47,13 +118,12 @@ export default function ModalContent({ index, title, name, data, handleClose }) 
         bgcolor: 'background.paper',
         boxShadow: 24,
         p: 4,
+        overflowY: 'auto',
       }}>
       <Typography variant='h6' component='h2'>
         {`#${index + 1} - ${title || name}`}
       </Typography>
-      <Typography sx={{ mt: 2 }}>
-        {renderJSON(data, fetchedData)}
-      </Typography>
+      <Typography sx={{ mt: 2 }}>{renderJSON(data, fetchedData)}</Typography>
       <Button
         onClick={handleClose}
         variant='contained'
