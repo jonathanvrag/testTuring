1cb136cf12b2d2605b7f1b8d425720a23c06c6d0
jonathanvra <jonathan.vra@gmail.com>
2024-08-02 21:52:12 -0500

    feat: Improve CardAlbum component to include custom styles and typography
    The CardAlbum component has been updated to include custom styles, such as a background filter, shadow, and rounded border. Additionally, a custom font has been added to improve the readability of the title. These changes improve the appearance and user experience by highlighting album cards in the app.

--- src/app/components/CardAlbum.jsx
+++ src/app/components/CardAlbum.jsx
@@ -2,12 +2,24 @@ import { Circle } from '@mui/icons-material';
 import { Avatar, Card, CardHeader } from '@mui/material';
 import React from 'react';
 
+const cardStyles = {
+  backdropFilter: 'blur(10px)',
+  backgroundColor: 'rgba(255, 255, 255, 0.3)',
+  boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)',
+  border: '1px solid rgba(255, 255, 255, 0.18)',
+  borderRadius: '10px',
+  maxWidth: 345,
+};
+
+const typographyStyles = {
+  color: 'black',
+  fontFamily: 'PT Sans Narrow, sans-serif',
+}
+
 export default function CardAlbum({ index, type }) {
   return (
     <Card
-      sx={{
-        maxWidth: 345,
-      }}>
+      sx={cardStyles}>
       <CardHeader
         avatar={
           <Avatar
@@ -17,7 +29,8 @@ export default function CardAlbum({ index, type }) {
             <Circle />
           </Avatar>
         }
-        title={`#${index + 1}`}></CardHeader>
+        title={`#${index + 1}`}
+        sx={typographyStyles}></CardHeader>
     </Card>
   );
 }
